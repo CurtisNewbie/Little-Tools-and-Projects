4ab349c518b2c1fb71e5b11a9ea061f9ec0f3180
Curtis <Zhuangyongj@gmail.com>
2019-09-15 17:33:50 +0100

    Fix existing bug, and add open generated file function
    Fix: the program does not exist when the width or height is less than 0
    
    Add function: the program will open the generated image after all if the os is windows.

--- QRGenerator/my-app/src/main/java/com/curtisnewbie/app/App.java
+++ QRGenerator/my-app/src/main/java/com/curtisnewbie/app/App.java
@@ -20,10 +20,12 @@ public class App {
     public static void main(String[] args) {
 
         // get console input
-        System.out.println("This is the QRCodeGenerator. Please enter the text that you want to encode:");
+        System.out.println("\n------------------------"
+                + "\n\nThis is the QRCodeGenerator. Please enter the text that you want to encode:"
+                + "\n\n------------------------");
         String text = in.nextLine();
 
-        System.out.println("\nPlease specify the width of the QR code image [whole number/ int]");
+        System.out.println("\nPlease specify the width of the QR code image [whole number/int]");
         int width = in.nextInt();
 
         System.out.println("\nPlease specify the height of the QR code image [whole number/int]");
@@ -31,26 +33,36 @@ public class App {
 
         if (width <= 0 || height <= 0) {
             System.out.println("Width and height must be greater than 0");
+            System.exit(0);
         }
 
         System.out.println("\nPlease specify the output path of the image");
         in.nextLine();
         String outputPath = in.nextLine();
 
-        // generate QR code image
         System.out.println("GeneratingQRIMage:...");
         try {
+            // generate QR code image
             generateQRImage(text, width, height, outputPath);
+            System.out.println("Done!");
+
+            // open the created images
+            String os = System.getProperty("os.name").toLowerCase();
+            if (os.contains("window")) {
+                System.out.println("Attempting to open the file");
+                openFile(outputPath);
+                System.out.println("The file is located at: " + "\"" + outputPath + "\"");
+            }
+
         } catch (WriterException e) {
             System.out.println("Failed to generate QR image, existing...");
             System.out.println("\n" + e.toString());
             System.exit(0);
         } catch (IOException e) {
-            System.out.println("Failed to write the image to the file system...");
             System.out.println("\n" + e.toString());
             System.exit(0);
         }
-        System.out.println("Done");
+
     }
 
     /**
@@ -73,5 +85,17 @@ public class App {
 
         // writer th image using image writer
         MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    /**
+     * Open the created file. This function only supports windows os, beacuse I use
+     * Windows (unfortunately).
+     * 
+     * @param p file path as a string.
+     */
+    public static void openFile(String p) throws IOException {
+
+        Runtime runtime = Runtime.getRuntime();
+        runtime.exec("cmd.exe /c \"" + p + "\"");
     }
 }
