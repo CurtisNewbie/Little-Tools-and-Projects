ba6635c879d8a1ba0a8925ace8608e87b013eb64
CurtisNewbie <zhuangyongj@gmail.com>
2020-04-16 22:04:59 +0800

    Fix bug

--- ffmpegConvert/FfmpegConvert.java
+++ ffmpegConvert/FfmpegConvert.java
@@ -3,6 +3,10 @@ import java.util.*;
 
 public class FfmpegConvert {
 
+    static char slash = isWinOS() ? '\\' : '/';
+    static String cli_name = isWinOS() ? "cmd.exe" : "bash";
+    static String parseAsString = isWinOS() ? "/c" : "-c";
+
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
             System.out.println("Three arguments: [0]input dir [1]output dir [2]target format");
@@ -19,24 +23,34 @@ public class FfmpegConvert {
             System.out.println("Output Dir is not a directory");
         }
 
-        List<String> inFiles = new ArrayList<>();
+        List<File> inFiles = new ArrayList<>();
         for (File f : inDir.listFiles())
-            inFiles.add(f.getAbsolutePath());
+            if (f.isFile())
+                inFiles.add(f);
+
         convert(inFiles, args[1], args[2]);
     }
 
-    static void convert(List<String> inFiles, String outDir, String format) throws Exception {
+    static void convert(List<File> files, String outDir, String format) throws Exception {
         System.out.println("Started at " + new Date().toString());
         Runtime runtime = Runtime.getRuntime();
         List<String> log = new ArrayList<String>();
-        for (String f : inFiles) {
-            String cmd = "ffmpeg -nostats -loglevel 0 -y -i " + f + " -vcodec copy -acodec copy " + outDir + "/"
-                    + fileName(f) + "." + format;
-            Process p = runtime.exec(new String[] { "bash", "-c", cmd });
+        for (File f : files) {
+            String outputFilename = outDir + slash + fileName(f.getName()) + "." + format;
+            String cmd = String.format("ffmpeg -y -i '%s' -c copy '%s'", f.getAbsolutePath(), outputFilename);
+            Process p = runtime.exec(new String[] { cli_name, parseAsString, cmd });
+
             if (p.waitFor() == 0) {
-                System.out.println("Convertion Sucess - " + f);
+                System.out.println("[Success]: " + outputFilename);
             } else {
-                log.add("Failed to convert" + f);
+                // try again without codec copy
+                System.out.println("[Failed]: " + outputFilename);
+                System.out.println("[Try again without codec copy]: " + outputFilename);
+                cmd = String.format("ffmpeg -y -i '%s' '%s'", f, outputFilename);
+                if (runtime.exec(new String[] { cli_name, parseAsString, cmd }).waitFor() == 0)
+                    System.out.println("[Success]: " + outputFilename);
+                else
+                    log.add("Failed to convert" + f);
             }
         }
         displayLog(log);
@@ -44,19 +58,16 @@ public class FfmpegConvert {
     }
 
     static String fileName(String path) {
-        int start = -1;
-        int end = -1;
-        for (int i = path.length() - 1; i > 0; i--) {
-            if (path.charAt(i) == '.' && end < 0) {
+        int start = 0;
+        int end = path.length();
+        for (int i = path.length() - 1; i >= 0; i--) {
+            if (path.charAt(i) == '.' && end == path.length()) {
                 end = i;
-            } else if (path.charAt(i) == '/') {
+            } else if (path.charAt(i) == slash) {
                 start = i + 1;
                 break;
             }
         }
-        if (start < 0 || end < 0)
-            throw new IllegalArgumentException("File name cannot be parsed");
-
         return path.substring(start, end);
     }
 
@@ -64,5 +75,9 @@ public class FfmpegConvert {
         for (String s : log) {
             System.out.println(s);
         }
+    }
+
+    private static boolean isWinOS() {
+        return System.getProperty("os.name").toLowerCase().contains("win");
     }
 }
